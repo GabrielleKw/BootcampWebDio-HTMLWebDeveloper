276af2195cebc85f0a17356d86158d20d2af450f
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-06-23 10:56:02 -0300

    Create MutiplicacaoSimples.js

---
+++ Introdução a Programação com JavaScript/MutiplicacaoSimples.js
@@ -0,0 +1,10 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+let total =  valor1 * valor2;
+print("PROD = " +    total  );
