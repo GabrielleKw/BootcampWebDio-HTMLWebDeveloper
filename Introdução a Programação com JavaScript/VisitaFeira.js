50414a3331e54efd31286ba3a71df1bc9d1b26bc
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-06-23 10:57:34 -0300

    Create VisitaFeira.js

---
+++ Introdução a Programação com JavaScript/VisitaFeira.js
@@ -0,0 +1,11 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+let line = gets().split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+let total = A + B ; 
+print("X = " + total);
